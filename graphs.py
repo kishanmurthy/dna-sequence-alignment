43efe774b6f2dedce4bd3c890f4d5e11f4f21954
Paritosh Singh <46970368+paritoshsinghrahar@users.noreply.github.com>
2021-11-25 21:49:37 -0800

    update

--- graphs.py
+++ graphs.py
@@ -86,7 +86,7 @@ def generate_graphs():
     plt.plot(memory_comp_df["peak_memory_basic"], label = 'peak_memory_basic')
     plt.plot(memory_comp_df["peak_memory_efficient"], label = 'peak_memory_efficient')
     plt.legend(loc="upper right")
-    plt.xlabel('Probelm Size')
+    plt.xlabel('Problem Size')
     plt.ylabel('Memory (KB)')
     plt.savefig('MemoryPlot.png',dpi=300)
 
@@ -95,8 +95,8 @@ def generate_graphs():
     plt.plot(time_comp_df["time_basic"], label = 'time_basic')
     plt.plot(time_comp_df["time_efficient"], label = 'time_efficient')
     plt.legend(loc="upper right")
-    plt.xlabel('Probelm Size')
-    plt.ylabel('Time(seconds)')
+    plt.xlabel('Problem Size')
+    plt.ylabel('Time (seconds)')
     plt.savefig('CPUPlot.png',dpi=300)
     plt.show()
     
